a34850e81cb97d7415a74da5a1737896452a73b4
JordanLevy <jordaneblevy@gmail.com>
2019-10-30 13:27:02 -0400

    Fixed geolocation search

--- js/geo.js
+++ js/geo.js
@@ -1,11 +1,20 @@
-function getLocation() {
+dest = "";
+
+function getLocation(d) {
+	dest = d;
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    window.alert("Unable to get position.")
+    navigator.geolocation.getCurrentPosition(showPosition, positionError);
+  } else {
+    alert("Unable to search using location.")
   }
 }
 
 function showPosition(position) {
-  window.alert("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+  alert("Searching for McMaster restrooms near coordinates:\nLatitude: " + position.coords.latitude +
+  "\nLongitude: " + position.coords.longitude);
+  window.location.href=dest;
+}
+
+function positionError() {
+	alert("Unable to search using location. Please allow the site to know your location, or try to search by criteria instead.");
 }
