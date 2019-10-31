31b1dc2bb09a0e1e95f0774f0226951449a74a6c
JordanLevy <jordaneblevy@gmail.com>
2019-10-31 16:32:22 -0400

    HTML/CSS validation

--- js/geo.js
+++ js/geo.js
@@ -3,7 +3,12 @@ dest = "";
 function getLocation(d) {
 	dest = d;
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition, positionError);
+    if(d == 'None') {
+      navigator.geolocation.getCurrentPosition(setLatLongCoords, currLocError);
+    }
+    else {
+      navigator.geolocation.getCurrentPosition(showPosition, searchError);
+    }
   } else {
     alert("Unable to search using location.")
   }
@@ -15,6 +20,15 @@ function showPosition(position) {
   window.location.href=dest;
 }
 
-function positionError() {
+function setLatLongCoords(position) {
+  document.getElementById('latitude').value = position.coords.latitude
+  document.getElementById('longitude').value = position.coords.longitude;
+}
+
+function searchError() {
 	alert("Unable to search using location. Please allow the site to know your location, or try to search by criteria instead.");
+}
+
+function currLocError() {
+  alert("Unable to retrieve current location. Please allow the site to know your location, or enter your coordinates manually instead.")
 }
