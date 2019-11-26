9532dd5b30e72fc74b57b40b4e84a131a2613703
JordanLevy <jordaneblevy@gmail.com>
2019-11-26 15:26:10 -0500

    hi

--- login.php
+++ login.php
@@ -1,88 +1,88 @@
-<!-- <?php
-// Initialize the session
-session_start();
+ <?php
+// // Initialize the session
+// session_start();
  
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-  header("location: search.php");
-  exit;
-}
+// // Check if the user is already logged in, if yes then redirect him to welcome page
+// if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+//   header("location: search.php");
+//   exit;
+// }
  
-// Include config file
-require_once "config.php";
+// // Include config file
+// require_once "config.php";
 
-$username = $password = "";
-$isError=false;
+// $username = $password = "";
+// $isError=false;
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    if(empty(trim($_POST["username"]))){
-    	$isError=true;
-        echo "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
+//     if(empty(trim($_POST["username"]))){
+//     	$isError=true;
+//         echo "Please enter username.";
+//     } else{
+//         $username = trim($_POST["username"]);
+//     }
     
-    if(empty(trim($_POST["password"]))){
-    	$isError=true;
-        echo "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
+//     if(empty(trim($_POST["password"]))){
+//     	$isError=true;
+//         echo "Please enter your password.";
+//     } else{
+//         $password = trim($_POST["password"]);
+//     }
     
-    // Validate credentials
-    if(!$isError){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+//     // Validate credentials
+//     if(!$isError){
+//         // Prepare a select statement
+//         $sql = "SELECT id, username, password FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+//         if($stmt = mysqli_prepare($link, $sql)){
+//             // Bind variables to the prepared statement as parameters
+//             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
-            // Set parameters
-            $param_username = $username;
+//             // Set parameters
+//             $param_username = $username;
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
+//             // Attempt to execute the prepared statement
+//             if(mysqli_stmt_execute($stmt)){
+//                 // Store result
+//                 mysqli_stmt_store_result($stmt);
                 
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
+//                 // Check if username exists, if yes then verify password
+//                 if(mysqli_stmt_num_rows($stmt) == 1){                    
+//                     // Bind result variables
+//                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+//                     if(mysqli_stmt_fetch($stmt)){
+//                         if(password_verify($password, $hashed_password)){
+//                             // Password is correct, so start a new session
+//                             session_start();
                             
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
+//                             // Store data in session variables
+//                             $_SESSION["loggedin"] = true;
+//                             $_SESSION["id"] = $id;
+//                             $_SESSION["username"] = $username;                            
                             
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            echo "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    echo "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
+//                             // Redirect user to welcome page
+//                             header("location: welcome.php");
+//                         } else{
+//                             echo "The password you entered was not valid.";
+//                         }
+//                     }
+//                 } else{
+//                     echo "No account found with that username.";
+//                 }
+//             } else{
+//                 echo "Oops! Something went wrong. Please try again later.";
+//             }
+//         }
         
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
+//         // Close statement
+//         mysqli_stmt_close($stmt);
+//     }
     
-    // Close connection
-    mysqli_close($link);
-}
-?> -->
+//     // Close connection
+//     mysqli_close($link);
+// }
+?> 
 
 <!DOCTYPE html>
 <html>
