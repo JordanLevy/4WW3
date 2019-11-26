d857244a4728cf45e231130aa28d9008a60340fd
JordanLevy <jordaneblevy@gmail.com>
2019-11-26 17:56:58 -0500

    hi

--- config.php
+++ config.php
@@ -31,8 +31,14 @@
 
 $connectionInfo = array("UID" => "jordan", "pwd" => "levy2019!", "Database" => "4ww3db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:4ww3dbserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-$conn.disconnect();
+try {
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	$conn.disconnect();
+} catch (Exception $e) {
+	$code = $e->getCode();
+	$msg = $e->getMessage();
+	echo $code.": ".$error_message."<br />";
+}
 
 ?>
