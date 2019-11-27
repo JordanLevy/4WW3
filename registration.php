93ab8b27c300ada030be95c73b0d5d98776c411c
JordanLevy <jordaneblevy@gmail.com>
2019-11-27 17:45:16 -0500

    Stuff

--- registration.php
+++ registration.php
@@ -2,13 +2,7 @@
 
 require_once "config.php";
 
-<<<<<<< HEAD
 $username = $password = $confirmPassword = $email = $dateOfBirth = "";
-=======
-echo "hello world!<br>";
-
-$username = $password = $confirmPassword = $email = $birthday = "";
->>>>>>> parent of 054765e... Removed more echo commands
 $isError = false;
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -68,8 +62,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			$notifications=1;
 		}
 
-		$dateOfBirth=date("Y-m-d H:i:s", $dateOfBirth)
-
 		echo $username
 		echo $password
 		echo $email
@@ -79,13 +71,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		if(!$isError) {
 			$params = array($username, $password, $email, $dateOfBirth, $notifications);
 			$query="INSERT INTO users (username, password, email, dateOfBirth, notifications) VALUES (?, ?, ?, ?, ?)";
-<<<<<<< HEAD
-=======
-			echo "Query: " . $query . "<br>";
-			echo "Params: " . $params . "<br>";
-			echo "Connection: " . $conn . "<br>";
-			var_dump($conn);
->>>>>>> parent of 054765e... Removed more echo commands
 			$result = sqlsrv_query($conn, $query, $params);
 			if( $result === false ) {
 				echo "ERROR<br>";
@@ -104,11 +89,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 // try to close the db connection at the end 
 try {
 	sqlsrv_close($conn);
-<<<<<<< HEAD
-=======
-	echo "database connected closed I think!<br>";
-	var_dump($conn);
->>>>>>> parent of 054765e... Removed more echo commands
 } catch (Exception $e) {
 	$code = $e->getCode();
 	$msg = $e->getMessage();
