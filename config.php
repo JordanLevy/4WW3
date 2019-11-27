4a296a70d41dd2a2b684ae771a717779fd176c09
JordanLevy <jordaneblevy@gmail.com>
2019-11-27 16:46:33 -0500

    Removed config echo commands

--- config.php
+++ config.php
@@ -1,18 +1,19 @@
 <?php
-echo "config page!<br>";
 
 $connectionInfo = array("UID" => "jordan", "pwd" => "levy2019!", "Database" => "4ww3db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:4ww3dbserver.database.windows.net,1433";
+$conn = false;
 
-try {
-	$conn = sqlsrv_connect($serverName, $connectionInfo);
-	echo "database connected I think!<br>";
-	var_dump($conn);
-} catch (Exception $e) {
-	$code = $e->getCode();
-	$msg = $e->getMessage();
-	echo $code.": ".$error_message."<br />";
+if ($conn != false) {
+	try {
+		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		var_dump($conn);
+	} catch (Exception $e) {
+		$code = $e->getCode();
+		$msg = $e->getMessage();
+		echo $code.": ".$error_message."<br />";
 
+	}
 }
 
 ?>
