17750037cffdf6e0ca032194f020664b8d5fd57d
JordanLevy <jordaneblevy@gmail.com>
2019-11-27 18:50:47 -0500

    idk

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-	header("location: search.php");
+	header("location: welcome.php");
 	exit;
 }
  
@@ -30,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $password = trim($_POST["password"]);
     }
 
-    //   credentials
+    //credentials
     if(!$isError){
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
