f4b00e2c3528a0fbe8d15c1aeb25c11ca18fb334
JordanLevy <jordaneblevy@gmail.com>
2019-11-30 16:51:03 -0500

    Test

--- search.php
+++ search.php
@@ -1,6 +1,3 @@
-<!-- link custom js file to use geolocation -->
-<script src="js/geo.js"></script>
-
 <?php
 
 require_once "config.php";
@@ -47,6 +44,50 @@ if(isset($_POST['submit'])){
     	//header("location:results_sample.php?terms=" . $searchTerms . "&rating=" . $star . "&men=" . $menCheckbox . "&women=" . $womenCheckbox . "&allGenders=" . $allGendersCheckbox );
     	$searchUrl = "results_sample.php?terms=" . $searchTerms . "&rating=" . $star . "&men=" . $menCheckbox . "&women=" . $womenCheckbox . "&allGenders=" . $allGendersCheckbox;
     	echo "<script type='text/javascript'>
+
+
+dest=\"\";
+
+//gets the longitude and latitude of the device
+function getLocation(d) {
+	console.log(d);
+	dest=d;
+	if (navigator.geolocation) {
+		if(d == 'None') {
+			navigator.geolocation.getCurrentPosition(setLatLongCoords, currLocError);
+		}
+		else {
+				navigator.geolocation.getCurrentPosition(byCrit, searchError);
+		}
+	} else {
+		alert(\"Unable to search using location.\")
+	}
+}
+
+//display the location and redirect to dest
+function byLoc(position) {
+	//document.getElementById('searchLatitude').value = position.coords.latitude
+	//document.getElementById('searchLongitude').value = position.coords.longitude;
+	document.location = dest \"&long=\" + position.coords.longitude + \"&lat=\" + position.coords.latitude;
+}
+
+//sets the latitude and longitude text boxes
+function setLatLongCoords(position) {
+	document.getElementById('latitude').value = position.coords.latitude
+	document.getElementById('longitude').value = position.coords.longitude;
+}
+
+//show an error on the search page
+function searchError() {
+	alert(\"Unable to search using location. Please allow the site to know your location, or try to search by criteria instead.\");
+}
+
+//show an error on the submission page
+function currLocError() {
+	alert(\"Unable to retrieve current location. Please allow the site to know your location, or enter your coordinates manually instead.\")
+}
+
+
     	getLocation('$searchUrl');
     	</script>";
 	}
