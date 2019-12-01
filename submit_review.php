5b66bcf515f6fb2ddddc89ce73eac781bf75de83
JordanLevy <jordaneblevy@gmail.com>
2019-12-01 18:21:25 -0500

    hi

--- submit_review.php
+++ submit_review.php
@@ -6,7 +6,6 @@
 	$reviewStar = $_POST['reviewStar'];
 	$reviewText = $_POST['reviewText'];
 	$bathroomId = $_POST['bathroomId'];
-	$avgRating = $_POST['avgRating'];
 
 	// print_r($_POST);
 	echo "reviewStar is " . $reviewStar . ". reviewText is " . $reviewText . ". Session id is " . $_SESSION["id"] . ". Bathroom id is " . $bathroomId;
@@ -21,6 +20,29 @@
 		print_r($errors);
 		echo "<br>";
 	    die();
+	}
+
+	//calculate the average rating
+	$params = array($bathroomId);
+	$query = "SELECT avg(Cast(rating as Float)) FROM reviews WHERE objectID=?";
+	$result = sqlsrv_query($conn, $query, $params);
+	//if the search didn't work
+	if( $result === false ) {
+		echo "ERROR<br>";
+		$errors=sqlsrv_errors();
+		echo "<br>";
+		print_r($errors);
+		echo "<br>";
+		die();
+	}
+	//if it's zero rows
+	if(sqlsrv_has_rows($result) != 1){
+		   echo "0 rows";
+	}else{
+		//get the query results
+		while($row = sqlsrv_fetch_array($result)){
+			$avgRating = $row[0];
+		}
 	}
 
 	$params = array($avgRating, $bathroomId);
