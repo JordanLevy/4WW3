747511feab494da84412664859bdeb0c8739803b
JordanLevy <jordaneblevy@gmail.com>
2019-12-01 16:10:00 -0500

    hi

--- submit_review.php
+++ submit_review.php
@@ -1,22 +1,24 @@
 <?php
 
 	session_start();
+	require_once "config.php";
 
 	$reviewStar = $_POST['reviewStar'];
 	$reviewText = $_POST['reviewText'];
 	$bathroomId = $_POST['bathroomId'];
 
-	// $params = array($_GET['id'], $_SESSION["id"], $reviewStar, $reviewText);
-	// $query="INSERT INTO reviews (objectID, userID, rating, description) VALUES (?, ?, ?, ?)";
-	// $result = sqlsrv_query($conn, $query, $params);
-	// if( $result === false ) {
-	// 	echo "ERROR<br>";
-	// 	$errors=sqlsrv_errors();
-	// 	echo "<br>";
-	// 	print_r($errors);
-	// 	echo "<br>";
-	//     die();
-	// }
 	// print_r($_POST);
 	echo "reviewStar is " . $reviewStar . ". reviewText is " . $reviewText . ". Session id is " . $_SESSION["id"] . ". Bathroom id is " . $bathroomId;
+
+	$params = array($bathroomId, $_SESSION["id"], $reviewStar, $reviewText);
+	$query="INSERT INTO reviews (objectID, userID, rating, description) VALUES (?, ?, ?, ?)";
+	$result = sqlsrv_query($conn, $query, $params);
+	if( $result === false ) {
+		echo "ERROR<br>";
+		$errors=sqlsrv_errors();
+		echo "<br>";
+		print_r($errors);
+		echo "<br>";
+	    die();
+	}
 ?>
