29afe5662247571155374d9955e8c85a2e7e2bcd
JordanLevy <jordaneblevy@gmail.com>
2019-12-01 20:17:50 -0500

    Test

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "ENV VARIABLE " . getenv("hello");
+
 $connectionInfo = array("UID" => "jordan", "pwd" => "levy2019!", "Database" => "4ww3db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:4ww3dbserver.database.windows.net,1433";
 $conn=false;
